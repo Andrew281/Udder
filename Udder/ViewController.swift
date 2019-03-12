8d72530f265bc4b3d8fe155e27c058fe8c94a991
Андрей Чеканников <andreychekannikov@MacBook-Air-Andrej.local>
2019-03-12 13:04:20 +0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Андрей Чеканников on 12/03/2019.
+//  Copyright © 2019 Andrey Chekannikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
